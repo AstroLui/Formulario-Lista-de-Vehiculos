8df6c9b73c33c74fe5e21b0c011a3417e2125f90
Astrou Lui <luisdaniel16an@gmail.com>
2024-05-17 19:43:38 -0400

    feat: star the structs project

---
+++ src/Formulario/BuildingFormulario.java
@@ -0,0 +1,9 @@
+package Formulario;
+public class BuildingFormulario {
+    public static void main(String[] args) 
+    {
+        BuildFormulario Formulario = new BuildFormulario();
+        Formulario.setVisible(true);
+    }
+    
+}
