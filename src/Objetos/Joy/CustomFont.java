13634eba7db705e1cac0f9b69bcff318816f583c
Astrou Lui <luisdaniel16an@gmail.com>
2024-05-19 11:39:50 -0400

    feat: adding the Fonts

---
+++ src/Objetos/Joy/CustomFont.java
@@ -0,0 +1,30 @@
+package Objetos.Joy;
+
+import java.awt.Font;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class CustomFont 
+{
+    private Font font = null;
+    public CustomFont(String nameFont)
+    {
+        try
+        {
+            InputStream is = new BufferedInputStream(new FileInputStream("src/Fonts/" + nameFont));
+            font = Font.createFont(Font.TRUETYPE_FONT, is);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage() + e.getLocalizedMessage());
+            font = new Font("Monospace", Font.PLAIN, 40);
+        }
+    }
+    
+    public Font myFont(int style, float Size)
+    {
+        Font rFont = font.deriveFont(style, Size);
+        return rFont;
+    }
+}
