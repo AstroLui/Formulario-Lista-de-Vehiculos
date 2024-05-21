1371ade255098bed4d292acf3c0084d2d1409b29
Astrou Lui <luisdaniel16an@gmail.com>
2024-05-21 07:32:01 -0400

    feat: adding the content in `Objects Keys`

--- src/Objetos/Keys/Car.java
+++ src/Objetos/Keys/Car.java
@@ -1,5 +1,51 @@
 package Objetos.Keys;
+
+import java.awt.Color;
+import java.util.Date;
+
 public class Car 
 {
+    private final String brand, model, licensePlate;
+    private People own;
+    private final Date yearCar;
+    private final Color color;
     
+    public Car(String brand, String model, String licensePlate, Date yearCar, Color colorCar)
+    {
+        this.brand = brand;
+        this.model = model;
+        this.licensePlate = licensePlate;
+        this.color = colorCar;
+        this.yearCar = yearCar;
+    }
+
+    public void asignOwn(People Own)
+    {
+        this.own = Own;
+    }
+    
+    public People getOwn()
+    {
+        return this.own;
+    }
+    public String getBrand()
+    {
+        return this.brand;
+    }
+    public String getModel()
+    {
+        return this.model;
+    }
+    public String getLicensePlate()
+    {
+        return this.licensePlate;
+    }
+    public Color getColorCar()
+    {
+        return this.color;
+    }
+    public Date getYearCar()
+    {
+        return this.yearCar;
+    }
 }
--- src/Objetos/Keys/People.java
+++ src/Objetos/Keys/People.java
@@ -1,4 +1,34 @@
 package Objetos.Keys;
-public class People {
+public class People 
+{
+    private final String name, lastName, dni, tlf, address;
+    public People(String name, String lastName, String dni, String tlf, String address)
+    {
+        this.name = name;
+        this.lastName = lastName;
+        this.dni = dni;
+        this.tlf = tlf;
+        this.address = address;
+    }
     
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+    public String getDni()
+    {
+        return this.dni;
+    }
+    public String getTlf()
+    {
+        return this.tlf;
+    }
+    public String getAddress()
+    {
+        return this.address;
+    }
 }
