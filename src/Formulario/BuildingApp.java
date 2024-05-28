3fe4e1fa8435ab9650f5005f364d9522c259b6d4
MoisesLondo <moiseslondo16@gmail.com>
2024-05-27 23:34:11 -0400

    fix: formulario y color

--- src/Formulario/BuildingApp.java
+++ src/Formulario/BuildingApp.java
@@ -1,6 +1,9 @@
 package Formulario;
+
+import java.text.ParseException;
+
 public class BuildingApp {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws ParseException 
     {
         BuildViewMain Formulario = new BuildViewMain();
         Formulario.setVisible(true);
